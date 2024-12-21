ead4d55f67b80cfb83fed6fac8ab51fafdc4d17e
arunkprabhu <arunkprabhu@gmail.com>
2024-12-21 12:11:02 +0530

    bug

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const handleClick = () => {
-  const createEl = document.createElement("h1");
+  const createEl = document.createElement("h");
   createEl.innerText = "clicked";
   createEl.id = "dynamic";
   document.body.appendChild(createEl);
 };
 
 const remeClick = () => {
-  const removeTag = document.getElementById("dynamic");
+  const removeTag = document.getElementById("dynamc");
 
   document.body.removeChild(removeTag);
 };

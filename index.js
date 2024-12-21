af8b6f893c2e33b40292704486c8560a62ae757e
arunkprabhu <arunkprabhu@gmail.com>
2024-12-21 12:13:28 +0530

    newww

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const handleClick = () => {
-  const createEl = document.createElement("h");
+  const createEl = document.createElement("h1");
   createEl.innerText = "clicked";
   createEl.id = "dynamic";
   document.body.appendChild(createEl);
 };
 
 const remeClick = () => {
-  const removeTag = document.getElementById("dynamc");
+  const removeTag = document.getElementById("dynamic");
 
   document.body.removeChild(removeTag);
 };

0867f867c77cb1668f140cd8846531bae9f06ed9
arunkprabhu <arunkprabhu@gmail.com>
2024-12-21 12:20:12 +0530

    new thing

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const handleClick = () => {
-  const createEl = document.createElement("h1");
+  const createEl = document.createElement("s");
   createEl.innerText = "clicked";
   createEl.id = "dynamic";
   document.body.appendChild(createEl);
 };
 
 const remeClick = () => {
-  const removeTag = document.getElementById("dynamic");
+  const removeTag = document.getElementById("dynaic");
 
   document.body.removeChild(removeTag);
 };
